e4884f6eece52be6fa970b0b1d81c4881dc007d9
김서규 <asap0208@coinone.com>
2022-10-12 12:13:35 +0900

    rsgrep 프로젝트 완료

---
+++ rsgrep/src/main.rs
@@ -0,0 +1,17 @@
+extern crate rsgrep;
+
+use rsgrep::{run, Config};
+use std::{env, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
